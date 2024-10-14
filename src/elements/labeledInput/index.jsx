99705f832b67958fa307b570255d778bb542afed
YankhairunisaZahra <111202214328@mhs.dinus.ac.id>
2024-10-14 17:40:23 +0700

    menambahkan warna dan font

---
+++ src/elements/labeledInput/index.jsx
@@ -0,0 +1,14 @@
+import input from "./input";
+import label from "./label";
+
+const labelInput = () => {
+    return (
+        <>
+        <label />
+        <input />
+        </>
+
+    );
+};
+
+export default labelInput;
---
+++ src/elements/labeledInput/label.jsx
@@ -0,0 +1,9 @@
+const label = () => {
+    return (
+        <label htmlFor="email" className="block text-sm mb-2">
+            Email Address
+        </label>
+    );
+};
+
+export default label;
