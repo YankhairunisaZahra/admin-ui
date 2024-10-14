6d37eaa3e04c971d1cafd551c11d7e6e8111d7c3
YankhairunisaZahra <111202214328@mhs.dinus.ac.id>
2024-10-14 16:15:39 +0700

    menambahkan warna dan font

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}",
+      ],
+    theme: {
+      extend: {
+            fontFamily: {
+              body: ['Inter'],
+              poppins: ['Poppins'],
+            },
+            colors: {
+              primary: "#299D91"
+            }
+          },
+        },
+    plugins: [],
+  }
