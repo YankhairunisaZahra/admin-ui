ff3870dcc0ed881636e17d387e5b7bea71a5ca9c
YankhairunisaZahra <111202214328@mhs.dinus.ac.id>
2024-10-28 17:03:30 +0700

    final

--- src/pages/signIn.jsx
+++ src/pages/signIn.jsx
@@ -1,7 +1,7 @@
 import FormSignIn from "../components/Fragments/FormSignIn";
 import AuthLayout from "../components/Layouts/AuthLayout";
 
-const SignInPages = () => {
+const SignInPage = () => {
   return (
     <AuthLayout type="sign in">
       <FormSignIn />
@@ -9,4 +9,4 @@ const SignInPages = () => {
   );
 };
 
-export default SignInPages;
+export default SignInPage;
