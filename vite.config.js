3665bd806c53140774a3183e7987e9f08d5cc580
YankhairunisaZahra <111202214328@mhs.dinus.ac.id>
2024-10-28 19:32:37 +0700

    error vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+    },
+  },
   resolve: {
     alias: {
       "@components": "/src/components",
