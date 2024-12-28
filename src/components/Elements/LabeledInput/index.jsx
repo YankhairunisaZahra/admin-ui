c373f39eeff8d9feb9967bf79a78b4f722ce607b
YankhairunisaZahra <111202214328@mhs.dinus.ac.id>
2024-12-28 12:16:24 +0700

    Adding Authentication

--- src/components/Elements/LabeledInput/index.jsx
+++ src/components/Elements/LabeledInput/index.jsx
@@ -1,14 +1,29 @@
+/* eslint-disable react/prop-types */
 import Input from "./input";
 import Label from "./label";
 
-const LabeledInput = (props) => {
-  const { label, name, type, placeholder } = props;
+// const LabeledInput = (props) => {
+//   const { label, name, type, placeholder } = props;
+//   return (
+//     <>
+//       <Label htmlFor={name}>{label}</Label>
+//       <Input name={name} type={type} placeholder={placeholder} />
+//     </>
+//   );
+// };
+
+const LabeledInput = ({ label, name, type, placeholder, register }) => {
   return (
     <>
       <Label htmlFor={name}>{label}</Label>
-      <Input name={name} type={type} placeholder={placeholder} />
+      <Input
+        name={name}
+        type={type}
+        placeholder={placeholder}
+        register={register}
+      />
     </>
-  );
-};
+  )
+}
 
 export default LabeledInput;
--- src/components/Elements/LabeledInput/input.jsx
+++ src/components/Elements/LabeledInput/input.jsx
@@ -1,16 +1,31 @@
-const Input = (props) => {
-    const { type, placeholder, name } = props;
-  
-    return (
-      <input
-        type={type}
-        className="py-3 ps-4 text-sm border rounded-md w-full bg-special-mainBg border-gray-03 text-gray-01 focus:border-black focus:outline-none focus:ring-0"
-        placeholder={placeholder}
-        name={name}
-        id={name}
-      />
-    )
-  }
-  
-  export default Input;
-  
+/* eslint-disable react/prop-types */
+// const Input = (props) => {
+//     const { type, placeholder, name } = props;
+
+//     return (
+//       <input
+//         type={type}
+//         className="py-3 ps-4 text-sm border rounded-md w-full bg-special-mainBg border-gray-03 text-gray-01 focus:border-black focus:outline-none focus:ring-0"
+//         placeholder={placeholder}
+//         name={name}
+//         id={name}
+//       />
+//     )
+//   }
+
+const Input = ({ name, type, placeholder, register }) => {
+  const variant = "bg-special-mainBg border border-gray-03";
+
+  return (
+    <input
+      type={type}
+      className={`${variant} py-3 ps-4 text-sm rounded-md w-full text-gray-01 focus:border-black focus:outline-none focus:ring-0`}
+      placeholder={placeholder}
+      name={name}
+      id={name}
+      {...register}
+    />
+  )
+}
+
+export default Input;
