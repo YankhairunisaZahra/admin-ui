999398f64048922355791255c8270f17c14f6945
YankhairunisaZahra <111202214328@mhs.dinus.ac.id>
2025-01-04 20:51:49 +0700

    Trying deploy

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true,
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
     },
   },
   resolve: {
