430f541cf2ca14ed46fe98ef64231368c54ee1d6
YankhairunisaZahra <111202214328@mhs.dinus.ac.id>
2025-01-04 21:52:34 +0700

    Trying deploy

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
@@ -10,8 +11,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      "@components": "/src/components",
-      "@data": "/src/data"
+      "@components": path.resolve(new URL('./src/components', import.meta.url).pathname),
+      "@data": path.resolve(new URL('./src/data', import.meta.url).pathname),
     },
   },
 });
